11bc9eb59d98bf79850116bd5d4e9f02ea9bf2ef
Om Rajput <omrajputt369@gmail.com>
2026-01-31 19:25:28 +0530

    Fix IfDemo.java: Added missing closing brace for class definition

---
+++ IfDemo.java
@@ -0,0 +1,10 @@
+public class IfDemo {
+    public static void main(String[] args) {
+
+        int x = 10;
+        if (x < 20) {
+            System.out.println("this is if statement");
+        }
+
+    }
+}
