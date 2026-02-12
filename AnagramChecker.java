e6bfb1d6ac1a1feeab5d8ab138aed701d7f3ca66
Om Rajput <omrajputt369@gmail.com>
2026-02-12 23:50:49 +0530

    Add AnagramChecker.java

---
+++ AnagramChecker.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramChecker {
+
+    public static boolean areAnagrams(String str1, String str2) {
+        // Remove whitespace and convert to lowercase
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Anagram Checker ===");
+        System.out.print("Enter first string: ");
+        String str1 = scanner.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = scanner.nextLine();
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("\n\"" + str1 + "\" and \"" + str2 + "\" are ANAGRAMS!");
+        } else {
+            System.out.println("\n\"" + str1 + "\" and \"" + str2 + "\" are NOT anagrams.");
+        }
+
+        scanner.close();
+    }
+}
