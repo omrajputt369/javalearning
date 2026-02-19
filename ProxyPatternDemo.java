af29755a10707e107472e974f21ae006f0c23422
Om Rajput <omrajputt369@gmail.com>
2026-02-19 15:39:26 +0530

    Add ProxyPatternDemo.java program (Batch 4)

---
+++ ProxyPatternDemo.java
@@ -0,0 +1,59 @@
+/**
+ * A Java program demonstrating the Proxy Design Pattern.
+ */
+
+// Image interface
+interface Image {
+    void display();
+}
+
+// Real Image class
+class RealImage implements Image {
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+
+    private void loadFromDisk(String fileName) {
+        System.out.println("Loading " + fileName + " from disk...");
+    }
+}
+
+// Proxy Image class
+class ProxyImage implements Image {
+    private RealImage realImage;
+    private String fileName;
+
+    public ProxyImage(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(fileName);
+        }
+        realImage.display();
+    }
+}
+
+// Client class
+public class ProxyPatternDemo {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test_image.jpg");
+
+        // Image will be loaded from disk
+        image.display();
+        System.out.println("");
+
+        // Image will not be loaded from disk (already loaded)
+        image.display();
+    }
+}
